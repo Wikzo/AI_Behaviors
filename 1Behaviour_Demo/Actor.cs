170f39325f855f4b0ecb646fa8cb9555e21ba6a0
Wikzo <Linkthewindwaker@gmail.com>
2014-03-11 00:12:51 +0100

    first

---
+++ 1Behaviour_Demo/Actor.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace _1Behaviour_Demo
+{
+    class Actor
+    {
+        public static List<Actor> Actors = new List<Actor>();
+        private static Random random = new Random();
+
+        public Color Color;
+        public Texture2D Texture;
+        public Vector2 Position;
+        public Vector2 Direction; // unit vector, point in a direction to move in conjunction with Speed
+        public float Speed;
+        public Vector2 Origin;
+
+        public static Vector2 GetRandomPosition(int rangeX, int rangeY)
+        {
+            return new Vector2(random.Next(rangeX), random.Next(rangeY));
+        }
+
+        public static Vector2 GetRandomDirection()
+        {
+            // random value between 0 to 2 pi circle
+            double rotation = random.NextDouble() * MathHelper.TwoPi;
+
+            // convert from radians to vector2
+            return new Vector2((float)Math.Cos(rotation), (float)Math.Sin(rotation));
+        }
+
+        public Actor(Color color, Texture2D texture)
+        {
+            Actors.Add(this);
+
+            this.Color = color;
+            this.Texture = texture;
+            this.Origin = new Vector2(texture.Width / 2, texture.Height / 2);
+
+            this.Position = new Vector2(Game1.ScreenWidth / 2,  Game1.ScreenHeight / 2);
+            this.Direction = new Vector2(-1, 0);
+        }
+
+        public void Update()
+        {
+            this.Position += this.Direction * this.Speed;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            // convert vector2 to radians
+            float rotation = (float) Math.Atan2(this.Direction.Y, this.Direction.X);// +MathHelper.PiOver2; // add 90 degree offset to orientate sprite correctly
+            
+            spriteBatch.Draw(this.Texture, this.Position, null, this.Color, rotation, this.Origin, 1f,
+                             SpriteEffects.None, 0f);
+
+            // screen wrapping physically
+            if (Position.X < -Texture.Width) Position.X += Game1.ScreenWidth;
+            if (Position.X > Game1.ScreenWidth) Position.X -= Game1.ScreenWidth;
+
+            if (Position.Y < -Texture.Height) Position.Y += Game1.ScreenHeight;
+            if (Position.Y > Game1.ScreenHeight) Position.Y -= Game1.ScreenHeight;
+
+            // SCREEN WRAPPING VISUAL START ------------------------
+            // check horizontal screen wrapping
+            if (this.Position.X < 0)
+            {
+                spriteBatch.Draw(this.Texture, new Vector2(this.Position.X + Game1.ScreenWidth, this.Position.Y), 
+                    null, this.Color, rotation, this.Origin, 1f,
+                             SpriteEffects.None, 0f);
+            }
+            else if (this.Position.X + this.Texture.Width > Game1.ScreenWidth)
+            {
+                spriteBatch.Draw(this.Texture, new Vector2(this.Position.X - Game1.ScreenWidth, this.Position.Y),
+                    null, this.Color, rotation, this.Origin, 1f,
+                             SpriteEffects.None, 0f);
+            }
+
+            // check vertical screen wrapping
+            if (this.Position.Y < 0)
+            {
+
+                spriteBatch.Draw(this.Texture, new Vector2(this.Position.X, this.Position.Y + Game1.ScreenHeight),
+                    null, this.Color, rotation, this.Origin, 1f,
+                             SpriteEffects.None, 0f);
+            }
+            else if (this.Position.Y + this.Texture.Height > Game1.ScreenHeight)
+            {
+                spriteBatch.Draw(this.Texture, new Vector2(this.Position.X, this.Position.Y - Game1.ScreenHeight),
+                    null, this.Color, rotation, this.Origin, 1f,
+                             SpriteEffects.None, 0f);
+            }
+
+            // SCREEN WRAPPING VISUAL END ------------------------
+        }
+    }
+}
---
+++ 1Behaviour_Demo/Game1.cs
@@ -0,0 +1,125 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+
+#endregion
+
+namespace _1Behaviour_Demo
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        public static int ScreenWidth = 800;
+        public static int ScreenHeight = 600;
+
+        public Game1()
+            : base()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferWidth = ScreenWidth;
+            graphics.PreferredBackBufferHeight = ScreenHeight;
+
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            Texture2D arrowTexture = Content.Load<Texture2D>("arrow");
+
+            Actor leader = new Actor(new Color(64, 255, 64), arrowTexture);
+            leader.Speed = 10;
+            leader.Direction = Actor.GetRandomDirection();
+            leader.Position = Actor.GetRandomPosition(ScreenWidth, ScreenHeight);
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            foreach (var actor in Actor.Actors)
+            {
+                actor.Update();
+
+                if (Keyboard.GetState().IsKeyDown(Keys.Left))
+                    actor.Position += new Vector2(-1, 0);
+
+                if (Keyboard.GetState().IsKeyDown(Keys.Right))
+                    actor.Position += new Vector2(1, 0);
+
+                if (Keyboard.GetState().IsKeyDown(Keys.Up))
+                    actor.Position += new Vector2(0, -1);
+
+                if (Keyboard.GetState().IsKeyDown(Keys.Down))
+                    actor.Position += new Vector2(0, 1);
+            }
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(new Color(96, 96, 111)); // blue-ish gray
+
+            spriteBatch.Begin();
+
+            foreach (Actor actor in Actor.Actors)
+            {
+                actor.Draw(spriteBatch);
+            }
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
